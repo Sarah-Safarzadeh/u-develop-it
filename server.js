a841432c4e15ce5bf22058771c8f9967c326f8f1
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-10-15 18:52:19 -0500

    routes to get a single candidate, all candidates, create candidate

--- server.js
+++ server.js
@@ -4,25 +4,79 @@ const app = express();
 const sqlite3 = require('sqlite3').verbose();
 
 // MIDDLEWARE
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Connect to database
 const db = new sqlite3.Database('./db/election.db', err => {
     if (err) {
-      return console.error(err.message);
+        return console.error(err.message);
     }
-  
+
     console.log('Connected to the election database.');
-  });
+});
 
 
 // Routes
-db.all(`SELET * FROM candidates`, (err, rows) => {
-    console.log(rows);
+
+// Delete a candidate
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+// ES5 function, not arrow function, to use this
+db.run(sql, params, function (err, result) {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result, this.lastID);
 });
 
-// Default response for any other request(Not Found) Catch all -- 404
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+})
+
+// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result, this, this.changes);
+//   });
+
+// Get single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates 
+                 WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+
+// Default response for any other request(Not Found) Catch all -- 404. keep as last/bottom route.
 app.use((req, res) => {
     res.status(404).end();
 });
@@ -31,6 +85,6 @@ app.use((req, res) => {
 // Start server after DB connection
 db.on('open', () => {
     app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+        console.log(`Server running on port ${PORT}`);
     });
-  });
+});
