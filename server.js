537562ffecffadc0fb3bae2aae016390c95607e2
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-10-14 22:04:08 -0500

    fixed server connection, fixed get route response

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const PORT = process.envPORT || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // MIDDLEWARE
@@ -7,13 +7,13 @@ app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
 // GET route
-app.get('/', (res, req) => {
+app.get('/', (req, res) => {
     res.json({
-        message: 'Hello World'
+      message: 'Hello World'
     });
-});
+  });
 
 // Initialize Express.js Server on 3001
 app.listen(PORT, () => {
-    console.log('Server running on port ${PORT}');
+    console.log(`Server running on port ${PORT}`);
 });
