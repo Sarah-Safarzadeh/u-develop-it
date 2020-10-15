769e4e05d21bc9f09343df9fc218830cf84cbf66
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-10-14 21:50:15 -0500

    Added app.listen to server.js

--- server.js
+++ server.js
@@ -5,3 +5,8 @@ const app = express();
 // MIDDLEWARE
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+// Initialize Express.js Server on 3001
+app.listen(PORT, () => {
+    console.log('Server running on port ${PORT}');
+});
