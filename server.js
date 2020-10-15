447e4d2a6cf019a78279882e5d8add1519cb3078
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-10-14 22:12:11 -0500

    added sqlite3 dependency, db middleware

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const sqlite3 = require('sqlite3').verbose();
 
 // MIDDLEWARE
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+// Connect to database
+const db = new sqlite3.Database('./db.election.db', err => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('connected to the election database');
+});
 
 // Routes
 
