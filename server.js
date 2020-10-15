95299b721edec6dadf768194becbd3ff152d1c19
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-10-14 21:47:58 -0500

    Added middleware to server.js

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 const express = require('express');
 const PORT = process.envPORT || 3001;
 const app = express();
+
+// MIDDLEWARE
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
