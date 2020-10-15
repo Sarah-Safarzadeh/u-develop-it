4c91466f95f2bc515463ecd8b9781530c2e516b4
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-10-14 21:52:50 -0500

    added GET route to server.js

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const app = express();
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+// GET route
+app.get('/', (res, req) => {
+    res.json({
+        message: 'Hello World'
+    });
+});
+
 // Initialize Express.js Server on 3001
 app.listen(PORT, () => {
     console.log('Server running on port ${PORT}');
