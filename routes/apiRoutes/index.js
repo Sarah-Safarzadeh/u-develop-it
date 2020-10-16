349dee6f958cb0993956fd11fa56a5c30251e3e7
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-10-16 18:56:26 -0500

    added middleware in index.js and ne notes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+//Middleware
 router.use(require('./candidateRoutes'));
+router.use(require('./voterRoutes'));
 
+
+//EXPORT
 module.exports = router;
