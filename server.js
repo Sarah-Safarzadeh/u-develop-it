fcc9633869bb44c9addf26cfa12712536f7b8a03
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-10-16 17:36:19 -0500

    Edited the 2 get route sql constants, added party_id

--- server.js
+++ server.js
@@ -35,7 +35,11 @@ db.run(sql, params, function (err, result) {
 
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+    AS party_name 
+    FROM candidates 
+    LEFT JOIN parties 
+    ON candidates.party_id = parties.id`;
     const params = [];
     db.all(sql, params, (err, rows) => {
         if (err) {
@@ -94,8 +98,12 @@ db.run(sql, params, function(err, result) {
 
 // Get single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates 
-                 WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id 
+             WHERE candidates.id = ?`;
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
         if (err) {
