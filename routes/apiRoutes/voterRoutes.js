a71b27c9d0fb5f38647095899a1af83406a82383
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-10-16 19:08:28 -0500

    added route to get individual voters

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -20,5 +20,23 @@ router.get('/voters', (req, res) => {
     });
 });
 
+// Route for Individual Voters
+router.get('/voter/:id', (req, res) => {
+    const sql = `SELECT * FROM voters WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
 //EXPORT
 module.exports = router;
