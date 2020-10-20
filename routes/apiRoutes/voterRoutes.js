b8badfc86a83398068527f9eb1c92211861663cd
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-10-19 22:35:52 -0500

    added voter post route in voterRoutes

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../../db/database');
 const inputCheck = require('../../utils/inputCheck');
 
+//GET ROUTES
 router.get('/voters', (req, res) => {
     const sql = `SELECT * FROM voters ORDER BY last_name`;
     const params = [];
@@ -34,6 +35,32 @@ router.get('/voter/:id', (req, res) => {
       res.json({
         message: 'success',
         data: row
+      });
+    });
+  });
+
+  // POST ROUTES
+  router.post('/voter', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'email');
+
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+    
+    const sql = `INSERT INTO voters (first_name, last_name, email) VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.email];
+  
+    db.run(sql, params, function(err, data) {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: body,
+        id: this.lastID
       });
     });
   });
